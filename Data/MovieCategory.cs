6a40f17f5b4464fee3e7f9eb21b10a480b1c765d
Jorge Ramirez <jorramirez_18@hotmail.com>
2022-08-09 19:19:11 -0600

    Se creo el modelo Movie y la carpeta Data

---
+++ Data/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data
+{
+    //cambiamo la CLASE por un ENUM
+    public enum MovieCategory
+    {
+        Action=1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
